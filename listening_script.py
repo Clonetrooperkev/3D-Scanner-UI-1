33151ec589b0116afa3a541f0884a74e63988980
Punny1m <punny1m@gmail.com>
2015-02-24 14:54:36 -0600

    Send and Listening script
    Send and listening script to grab jpg files from the pi, modified from
    original to make it changable

---
+++ listening_script.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import socket
+import struct
+import fcntl
+import subprocess
+import sys
+import time
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+def get_ip_address(ifname):
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+return socket.inet_ntoa(fcntl.ioctl(
+s.fileno(),
+0x8915, # SIOCGIFADDR
+struct.pack('256s', ifname[:15])
+)[20:24])
+
+id = get_ip_address('eth0')
+
+ip1, ip2, ip3, ip4 = id.split('.')
+
+print 'ID: ' + ip4 
+
+#create an options file, this file should containt the parameters for the raspistill image cmd
+optionfile = open('/server/options.cfg','r')
+options = optionfile.readline()
+optionfile.close()
+print "optons: " + options
+
+while True:
+data = sock.recv(10240)
+data = data.strip()
+if data == "reboot":
+print "rebooting..."
+cmd = 'sudo reboot'
+pid = subprocess.call(cmd, shell=True) 
+else:
+print "shooting " + data
+time.sleep(random.choice([0,1,2,3,4,5,6,7,8,9,10]))
+cmd = 'raspistill -o /tmp/photo.jpg ' + options 
+pid = subprocess.call(cmd, shell=True)
+print "creating directory"
+cmd = 'mkdir /server/3dscan/' + data 
+pid = subprocess.call(cmd, shell=True)
+print "copy image"
+cmd = 'cp /tmp/photo.jpg /server/3dscan/' + data + "/" + data + "_" + ip4 + '.jpg'
+pid = subprocess.call(cmd, shell=True)
+print "photo uploaded"
---
+++ send_script.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+import time
+
+#print 'photo name:'
+#n = sys.stdin.readline()
+#n = n.strip('\n')
+print ("Photo name is: %s" % str(sys.argv[1]))
+#photo name
+n = str(sys.argv[1])
+
+#timer
+a = int(sys,argv[2])
+
+time.sleep(a)
+
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+sock.sendto(n, (MCAST_GRP, MCAST_PORT))
